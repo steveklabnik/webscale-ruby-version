74404e325617c8fca492934fcb8c613bde9e944e
Steve Klabnik <steve@steveklabnik.com>
2011-12-11 19:32:53 -0500

    dumb rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lol::Application.config.secret_token = 'fa20d029025b71a5691ddcf2997a7b794ecfa537f8813a91a15ea041c57746c123c4e62dc5b84f1fd90e2b24418e4fd9d90b4ad433ff85746ffc561f3910086a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Lol::Application.config.session_store :cookie_store, key: '_lol_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Lol::Application.config.session_store :active_record_store
